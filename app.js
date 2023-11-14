d1dab194faa820da20634ca071977551fbbde887
helinter <helarandaragan999@gmail.com>
2023-11-14 15:16:15 +0000

    logger

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const handleErrors = require('./middlewares/errorMiddleware'); // Импортируем мидлвэр для обработки ошибок
+const { requestLogger, errorLogger } = require('./logger/logger');
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/mestodb';
+
+app.use(requestLogger);
 
 app.use(helmet());
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -48,6 +51,7 @@ app.use((req, res, next) => {
 
 // Подключаем мидлвэр для обработки ошибок
 app.use(handleErrors);
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
